fba01bbf4dc39ef97903742ba9ee88102f3805fd
Jose A Raposo <joseantonio.raposo.perez@iesdonana.org>
2023-10-03 00:24:18 +0200

    A lo bestia

--- edad.php
+++ edad.php
@@ -11,19 +11,61 @@
 const MES = [1=> 'Enero', 'Febrero','Marzo','Abril','Mayo',
     'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
 
+$anyo = (int)date('Y');
+
+
 ?>
 <form action="" method="get">
     <label for="dia">Fecha de nacimiento:</label>
     <select name="dia" id="dia">
         <?php for ($i = 1; $i<=31;$i++): ?> 
             <option value="<?= $i ?>"><?= $i ?></option>
-        <?php endfor ?>
+        <?php 
+        $diafecha = $i;
+        endfor ?>
     </select>
+
     <select name="mes" id="mes">
         <?php foreach (MES as $m => $nombre_mes): ?>
             <option value="<?=$m ?>"><?= $nombre_mes ?></option>
         <?php endforeach ?>
     </select>
+
+    <select name="anyo" id="anyo">
+        <?php for ($a = $anyo; $a >= ($anyo - 50); $a--): ?>
+            <option value="<?= $a ?>"><?= $a ?></option>
+        <?php endfor ?>
+        
+    </select>
+    <button type="submit">Calcular</button>
+
+
+    <?php 
+    $diafecha =$_GET["dia"];
+    $mesfecha = $_GET["mes"];
+    $anyofecha = $_GET["anyo"];
+
+ 
+
+        if (checkdate($mesfecha,$diafecha,$anyofecha)){
+            if ($mesfecha > date('m')){
+                echo(($anyo - $anyofecha) - 1);
+            } elseif ($mesfecha < date('m')){
+                echo ($anyo - $anyofecha);
+            }else {
+                if($diafecha > date('d')){
+                    echo ($anyo - $anyofecha);
+                }elseif ($diafecha < date('d')) {
+                    echo(($anyo - $anyofecha ) - 1);
+                }else{
+                    echo($anyo - $anyofecha);
+                    echo ("Feliz Cumpleaños");
+                }
+            }
+        } else {
+            echo("ERROR DE FECHA");
+        }
+    ?>
 </form>
 </body>
 </html>
